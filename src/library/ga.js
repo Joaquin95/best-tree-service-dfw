41ca7c4e6db81c5afeade2acc21f95599a892814
Joaquin <Mintinvestments95@gmail.com>
2025-09-02 15:35:44 -0500

    GA4

--- src/library/ga.js
+++ src/library/ga.js
@@ -1,17 +1,17 @@
-// export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID;
+export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID;
 
-// // Log a page view
-// export const pageview = (url) => {
-//   if (!window.gtag) return;
-//   window.gtag('config', GA_TRACKING_ID, { page_path: url });
-// };
+// Log a page view
+export const pageview = (url) => {
+  if (!window.gtag) return;
+  window.gtag('config', GA_TRACKING_ID, { page_path: url });
+};
 
-// // Log a custom event
-// export const event = ({ action, category, label, value }) => {
-//   if (!window.gtag) return;
-//   window.gtag('event', action, {
-//     event_category: category,
-//     event_label: label,
-//     value,
-//   });
-// };
+// Log a custom event
+export const event = ({ action, category, label, value }) => {
+  if (!window.gtag) return;
+  window.gtag('event', action, {
+    event_category: category,
+    event_label: label,
+    value,
+  });
+};
