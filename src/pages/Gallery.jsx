0f13f22d1bcc75ef1100a795057d8a2a632be09e
Joaquin <Mintinvestments95@gmail.com>
2025-09-02 10:49:37 -0500

    updating images

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -5,61 +5,47 @@ import EstimateForm from "./EstimateForm";
 import "../index.css";
 
 export default function Gallery() {
-
-
   return (
     <main>
       ;
       <section className="gallery-section">
         <h2>Our Work In Action</h2>
         <h3>
-        Explore how our team delivers safe, efficient, and professional tree services — 
-    from urgent removals to detailed precision trimming.
+          Explore how our team delivers safe, efficient, and professional tree
+          services — from urgent removals to detailed precision trimming.
         </h3>
         <div className="gallery-grid">
           <div className="gallery-item">
             <h3>Before Tree Removal</h3>
-            <img
-              src="/images/Tree-Cutting.jpg"
-              alt="Before Tree Removal"
-            />
-          </div>
-          <div className="gallery-item">
-            <h3>After Tree Removal</h3>
-            <img
-              src="/images/Tree-Cutting3.jpg"
-              alt="After Tree Removal"
-            />
+            <img src="/images/Tree-Cutting.jpg" alt="Before Tree Removal" />
           </div>
           <div className="gallery-item">
             {" "}
-            <h3>Professional Chipper Truck</h3>
-            <img src="/images/tree-cutting.jpg" alt="Chipper Truck" />
+            <h3>Process</h3>
+            <img src="/images/tree-cutting2.jpg" alt="Cutting Process" />
+          </div>
+          <div className="gallery-item">
+            <h3>After Tree Removal</h3>
+            <img src="/images/Tree-Cutting3.jpg" alt="After Tree Removal" />
           </div>
           <div className="gallery-item">
             {" "}
             <h3>Stump Grinding Process</h3>
-            <img
-              src="/images/StumpGrinding.jpg"
-              alt="Stump Grinder"
-            />
+            <img src="/images/StumpGrinding.jpg" alt="Stump Grinder" />
           </div>
-          <div className="gallery-item">
+          {/* <div className="gallery-item">
             {" "}
             <h3>The Team</h3>
-            <img
-              src="/images/"
-              alt="Stump Grinder"
-            />
-          </div>
+            <img src="/images/" alt="Stump Grinder" />
+          </div> */}
           {/* Add more items as needed */}
         </div>
 
         <section className="cta-section">
           <h3>Ready to Remove or Trim Your Trees?</h3>
           <h3>
-           Request your free estimate today. Our team provides fast, reliable, and 
-      professional service with no obligation.
+            Request your free estimate today. Our team provides fast, reliable,
+            and professional service with no obligation.
           </h3>
           <EstimateForm />
         </section>
