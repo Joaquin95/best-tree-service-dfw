c08254a31a785facb2fcea1bd3f89fd05433ce09
Joaquin Morales <mintinvestments95@gmail.com>
2026-05-21 07:36:41 -0500

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,55 @@
-import { BrowserRouter, Routes, Route, useLocation
- } from "react-router-dom";
-import React from "react";  
-import Home from "./pages/Home.jsx";
-import Footer from "./components/Footer";
-import Navbar from "./components/Navbar.jsx";
-import "leaflet/dist/leaflet.css";
-import ThankYou from "./pages/ThankYou.jsx";
-import Services from "./pages/Services.jsx";
-import Gallery from "./pages/Gallery.jsx";
-import StumpGrinding from "./pages/StumpGrinding.jsx";
-import TreeRemoval from "./pages/TreeRemoval.jsx";
-import TreeTrimming from "./pages/TreeTrimming.jsx";
-import EstimateForm from "./pages/EstimateForm.jsx";
-import ServiceArea from "./pages/ServiceArea.jsx";
-import Fences from "./pages/Fences.jsx";
-import { useEffect } from "react";
+// import { BrowserRouter, Routes, Route, useLocation
+//  } from "react-router-dom";
+// import React from "react";  
+// import Home from "./pages/Home.jsx";
+// import Footer from "./components/Footer";
+// import Navbar from "./components/Navbar.jsx";
+// import "leaflet/dist/leaflet.css";
+// import ThankYou from "./pages/ThankYou.jsx";
+// import Services from "./pages/Services.jsx";
+// import Gallery from "./pages/Gallery.jsx";
+// import StumpGrinding from "./pages/StumpGrinding.jsx";
+// import TreeRemoval from "./pages/TreeRemoval.jsx";
+// import TreeTrimming from "./pages/TreeTrimming.jsx";
+// import EstimateForm from "./pages/EstimateForm.jsx";
+// import ServiceArea from "./pages/ServiceArea.jsx";
+// import Fences from "./pages/Fences.jsx";
+// import { useEffect } from "react";
 
-function RouteChangeTracker() {
-  const location = useLocation();
+// function RouteChangeTracker() {
+//   const location = useLocation();
 
-  useEffect(() => {
-    if (typeof window.gtag === "function") {
-      window.gtag("event", "page_view", {
-        page_path: location.pathname + location.search,
-      });
-    }
-  }, [location]);
+//   useEffect(() => {
+//     if (typeof window.gtag === "function") {
+//       window.gtag("event", "page_view", {
+//         page_path: location.pathname + location.search,
+//       });
+//     }
+//   }, [location]);
 
-  return null;
-}
+//   return null;
+// }
 
 
-export default function App() {
-  return (
-    <BrowserRouter>
-      <RouteChangeTracker />
+// export default function App() {
+//   return (
+//     <BrowserRouter>
+//       <RouteChangeTracker />
 
-     <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/gallery" element={<Gallery />} />
-        <Route path="/estimate" element={<EstimateForm onSuccess={() => {}} />} />
-        <Route path="/service-area" element={<ServiceArea />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/fences" element={<Fences />} />
-        <Route path="/stump-grinding" element={<StumpGrinding />} />
-        <Route path="/tree-removal"   element={<TreeRemoval />} />
-        <Route path="/tree-trimming"  element={<TreeTrimming />} />
-        <Route path="/thank-you" element={<ThankYou />} />
-      </Routes>
-    <Footer />
-    </BrowserRouter>
-  );
-}
+//      <Navbar />
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/gallery" element={<Gallery />} />
+//         <Route path="/estimate" element={<EstimateForm onSuccess={() => {}} />} />
+//         <Route path="/service-area" element={<ServiceArea />} />
+//         <Route path="/services" element={<Services />} />
+//         <Route path="/fences" element={<Fences />} />
+//         <Route path="/stump-grinding" element={<StumpGrinding />} />
+//         <Route path="/tree-removal"   element={<TreeRemoval />} />
+//         <Route path="/tree-trimming"  element={<TreeTrimming />} />
+//         <Route path="/thank-you" element={<ThankYou />} />
+//       </Routes>
+//     <Footer />
+//     </BrowserRouter>
+//   );
+// }
